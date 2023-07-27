5946d09e463197e93956b4e147eaffaf67c4d523
Kalash Jain <kalashjain54@gmail.com>
2023-07-28 02:10:36 +0530

    with nakli animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,50 +1,138 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Hero = () => {
+const Node = ({ position }) => {
   return (
     <div
       style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: '-1',
-        overflow: 'hidden',
-        backgroundColor: '#333333', // Change the background color
+        position: 'absolute',
+        width: '3px',
+        height: '3px',
+        backgroundColor: 'rgba(66, 215, 245, 0.5)',
+        borderRadius: '50%',
+        left: position.x,
+        top: position.y,
       }}
-    >
-      {/* Astro */}
-      {/* <div
-        style={{
-          position: 'absolute',
-          width: '20px',
-          height: '20px',
-          backgroundColor: '#85fffb',
-          borderRadius: '50%',
-          animation: 'astroAnimation 2s infinite',
-        }}
-        className="astro" // Apply Tailwind classes for the "Astro" object */}
-      {/* /> */}
+      className="node"
+    />
+  );
+};
 
-      {/* Nodes */}
-      {Array.from({ length: 100 }, (_, i) => (
-        <div
-          key={i}
-          style={{
-            position: 'absolute',
-            width: '3px',
-            height: '3px',
-            backgroundColor: 'rgba(66, 215, 245, 0.5)',
-            borderRadius: '50%',
-            animation: 'nodeAnimation 3s infinite',
-            left: `${Math.random() * 100}%`, // Randomize node position
-            top: `${Math.random() * 100}%`,
-          }}
-          className="node" // Apply Tailwind classes for the "Node" objects
-        />
+const Astro = ({ position }) => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        width: '10px',
+        height: '10px',
+        backgroundColor: '#85fffb',
+        borderRadius: '50%',
+        left: position.x,
+        top: position.y,
+        // transition: 'left 0.3s ease, top 0.3s ease', // Add ease effect to Astro movement
+      }}
+      className="astro"
+    />
+  );
+};
+
+const Hero = () => {
+  const [nodes, setNodes] = useState([]);
+  const [astroPosition, setAstroPosition] = useState({ x: 0, y: 0 });
+  const astroRef = useRef(null);
+
+  useEffect(() => {
+    // Creating nodes with random positions for demonstration purposes
+    const nodesData = Array.from({ length: 300 }, (_, i) => ({
+      position: {
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+      },
+    }));
+    setNodes(nodesData);
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setAstroPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const findClosestNodes = () => {
+    const lineRadius = 80; // Set the line connecting radius here
+    const sortedNodes = nodes.map((node) => ({
+      ...node,
+      distance: Math.sqrt((astroPosition.x - node.position.x) ** 2 + (astroPosition.y - node.position.y) ** 2),
+    }));
+    sortedNodes.sort((a, b) => a.distance - b.distance);
+
+    return sortedNodes.filter((node) => node.distance <= lineRadius);
+  };
+
+  useEffect(() => {
+    const astroEaseMove = setInterval(() => {
+      const astroNode = astroRef.current;
+      if (astroNode) {
+        const cx = (astroPosition.x + astroNode.getBoundingClientRect().left) / 2;
+        const cy = (astroPosition.y + astroNode.getBoundingClientRect().top) / 2;
+        astroNode.style.left = `${cx}px`;
+        astroNode.style.top = `${cy}px`;
+      }
+    }, 30); // Adjust the interval to control the smoothness
+
+    return () => clearInterval(astroEaseMove);
+  }, [astroPosition]);
+
+  return (
+    <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: '-1', overflow: 'hidden' }}>
+    {/* Background */}
+    <div style={{ backgroundColor: '#333333', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }} />
+
+    {/* Nodes */}
+    <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}>
+      {nodes.map((node, index) => (
+        <Node key={index} position={node.position} />
       ))}
     </div>
+
+    {/* Astro */}
+    <Astro ref={astroRef} position={astroPosition} />
+
+    {/* Lines */}
+    <svg
+      style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, pointerEvents: 'none' }}
+      width="100%"
+      height="100%"
+    >
+      {/* Updated line path calculations */}
+      {findClosestNodes().map((node, index) => {
+        const dx = node.position.x - astroPosition.x;
+        const dy = node.position.y - astroPosition.y;
+        const cx1 = (astroPosition.x + node.position.x) / 2;
+        const cy1 = (astroPosition.y + node.position.y) / 2;
+        const cx2 = (astroPosition.x + node.position.x) / 2;
+        const cy2 = (astroPosition.y + node.position.y) / 2;
+
+        // Create a curved path string using cubic Bezier curve
+        const curvePath = `M${astroPosition.x},${astroPosition.y} C${cx1},${cy1} ${cx2},${cy2} ${node.position.x},${node.position.y}`;
+
+        return (
+          <path
+            key={index}
+            d={curvePath}
+            stroke="rgba(66, 215, 245, 0.5)"
+            fill="transparent"
+            // style={{ strokeWidth: 0.5, transition: 'stroke 0.3s ease' }} // Set the strokeWidth and ease effect
+          />
+        );
+      })}
+    </svg>
+  </div>
   );
 };
 
